b72e97ac72798ac928b383389c8481857462de24
beh_rooz <bmb.mm7@gmail.com>
2023-10-27 18:33:55 +0330

    new profile text

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -2,17 +2,18 @@ export default function Profile(){
 
 
   return (
-      <div className={`py-10`}>
-        I am interested in FrontEnd. And
-        in particular, I follow ReactJs
-        and NextJs. Clean coding is
-        important to me and I try to
-        follow its principles.
-        I attach special importance to
-        training and acquiring skills and
-        personal development. And I
-        find teamwork more helpful in
-        reaching this goal.
+      <div className={`py-10 text-lg`}>
+        I am interested in FrontEnd. And in particular, I follow ReactJs and NextJs. Clean coding is important to me and I try to follow its principles.I attach special importance to training and acquiring skills and personal development. And I find teamwork more helpful in reaching this goal.
+          <ul className={`list-disc ps-4 my-4`}>
+              <li>3+ years of React js experience</li>
+              <li>Strong Proficient and experience with React js and Next Js.</li>
+              <li>Strong Proficiency HTML, CSS, and JavaScript.</li>
+              <li>Proficient with implementing Restful APIs.</li>
+              <li>Proficient state management architecture Context, Redux, Redux toolkit</li>
+              <li>Solid understanding Git</li>
+              <li>Solid understanding tailwind,MUI</li>
+              <li>Strong communication, follow-up, and problem-solving skills.</li>
+          </ul>
       </div>
   )
 }
